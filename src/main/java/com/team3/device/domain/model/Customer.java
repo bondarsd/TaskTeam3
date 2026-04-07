cb995e14be3741ac3e9ae337fa0aab4cdca6e002
g3nri <rodionssokolovs@gmail.com>
2026-04-07 17:13:04 +0300

    [om-004] add Customer entity

---
+++ src/main/java/com/team3/device/domain/model/Customer.java
@@ -0,0 +1,27 @@
+package com.team3.device.domain.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+}
