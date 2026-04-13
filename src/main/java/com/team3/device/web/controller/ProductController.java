eae11a121ee0fa149fedd5f5c84f8f7031b5242b
deniss.bondars <deniss.bondars@accenture.com>
2026-04-13 13:25:20 +0300

    [om-005] - Update ProductController: remove customerId endpoint and add GET /products

--- src/main/java/com/team3/device/web/controller/ProductController.java
+++ src/main/java/com/team3/device/web/controller/ProductController.java
@@ -5,7 +5,6 @@ import com.team3.device.web.dto.CreateProductRequest;
 import com.team3.device.web.dto.ProductResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,8 +29,8 @@ public class ProductController {
         return productApplicationService.createProduct(request);
     }
 
-    @GetMapping("/customer/{customerId}")
-    public List<ProductResponse> getProductsByCostumerId(@PathVariable Long customerId) {
-        return productApplicationService.getProductsByCustomerId(customerId);
+    @GetMapping
+    public List<ProductResponse> getAllProducts() {
+        return productApplicationService.getAllProducts();
     }
 }
