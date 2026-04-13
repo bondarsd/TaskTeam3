6da15ce706dadd543a416f2accf93855a5b6101e
deniss.bondars <deniss.bondars@accenture.com>
2026-04-13 11:06:06 +0300

    [om-005] - Add DTOs for product creation and response

---
+++ src/main/java/com/team3/device/web/dto/CreateProductRequest.java
@@ -0,0 +1,39 @@
+package com.team3.device.web.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class CreateProductRequest {
+
+    @NotNull
+    private Long customerId;
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    private String brand;
+
+    @NotNull
+    @Positive
+    private BigDecimal price;
+
+    private String wifiStandard;
+
+    private Integer maxSpeedMbps;
+
+    private String frequencyBand;
+
+    private Integer lanPorts;
+
+    private Integer wanPorts;
+}
---
+++ src/main/java/com/team3/device/web/dto/ProductResponse.java
@@ -0,0 +1,31 @@
+package com.team3.device.web.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.math.BigDecimal;
+
+@Getter
+@AllArgsConstructor
+public class ProductResponse {
+
+    private Long id;
+
+    private Long customerId;
+
+    private String name;
+
+    private String brand;
+
+    private BigDecimal price;
+
+    private String wifiStandard;
+
+    private Integer maxSpeedMbps;
+
+    private String frequencyBand;
+
+    private Integer lanPorts;
+
+    private Integer wanPorts;
+}
