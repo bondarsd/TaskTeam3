68109c80fb339f46499cc124acbbc84df8764116
deniss.bondars <deniss.bondars@accenture.com>
2026-04-13 11:09:15 +0300

    [om-005] - Add ProductRepository interface

---
+++ src/main/java/com/team3/device/domain/repository/ProductRepository.java
@@ -0,0 +1,15 @@
+package com.team3.device.domain.repository;
+
+import com.team3.device.domain.model.Product;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProductRepository {
+
+    Product save(Product product);
+
+    List<Product> findByCustomerId(Long customerId);
+
+}
